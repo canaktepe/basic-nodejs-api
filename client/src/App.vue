923c4b7f8cf7356c87bf27eb5ba2c9daf34ebc87
Can Aktepe <aktepecan@gmail.com>
2019-12-12 23:24:58 +0300

    vuejs integration as client application.

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about" id="app">
+    <ul v-if="products.length > 0">
+      <li v-for="(product,index) in products" :key="index">{{ product.name }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import api from '@/services/api';
+
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    this.fetchPosts();
+  },
+  methods: {
+    fetchPosts() {
+      api().get('/api/products').then((result) => {
+        this.products = result.data;
+      });
+    },
+  },
+};
+</script>
